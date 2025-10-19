b4c41bcee2aaf8be7b1f4b041b2d2bffbb13ef28
tkitchenjr <tkitchenjr@gmail.com>
2025-10-19 11:09:43 -0500

    user methods created

--- app/domain/User.py
+++ app/domain/User.py
@@ -1,7 +1,6 @@
 from rich.console import Console
 from rich.table import Table 
 from typing import Dict
-import db
 
 _console = Console()
 
@@ -17,12 +16,15 @@ class User:
 
 # define function to view users in a table
 def view_users(users: list[User]) -> None:
-	table = Table (title="User List")
-	table.add_column("Username", justify = "center", style="orange", no_wrap=True)
-	table.add_column("First Name", justify = "center", style="orange", no_wrap=True)
-	table.add_column("Last Name", justify= "center", style= "orange", no_wrap=True)
-	table.add_column("Balance", justify= "center", style="green", no_wrap=True)
-	
+	table = Table(title="User List")
+	table.add_column("Balance", justify="center", style="green", no_wrap=True)
+	table.add_column("Username", justify="center", style='yellow', no_wrap=True)
+	table.add_column("First Name", justify="center", style='yellow', no_wrap=True)
+	table.add_column("Last Name", justify="center", style='yellow', no_wrap=True)
+	for user in users:
+		if user:
+			table.add_row(user.username, user.firstname, user.lastname, f"{user.balance:.2f}")
+	_console.print(table)
 
 # define function to add new user
 def add_user(username:str, password:str, firstname:str, lastname:str, balance:float) -> User:
@@ -45,6 +47,7 @@ def add_user(username:str, password:str, firstname:str, lastname:str, balance:fl
 
 # define function to delete user
 def delete_user(username:str) -> None:
+	import db
 	user = db.query_user(username)
 	if not user:
 		raise Exception ("User not found")
