3b8f52780e2f845c655639559223649d643cdb53
tkitchenjr <tkitchenjr@gmail.com>
2025-10-19 09:44:02 -0500

    meant to be user methods but defined user methods and established login operation with navigation to menus

--- app/db.py
+++ app/db.py
@@ -5,3 +5,53 @@ from domain.Portfolio import Portfolio
 from domain.Security import Security
 
 
+#create User list
+users = [
+    {
+        "username": "admin",
+        "password": "admin",
+        "firstname": "Admin",
+        "lastname": "Admin",
+        "balance": 100000.00
+    }
+]
+
+#create query user method
+def query_user(username: str) -> User|None:
+    for user in users:
+        if user["username"] == username:
+            return User(**user)
+    return None
+
+#create Portfolio list
+portfolios = [
+    {
+        "portfolio_id": 1,
+        "name": "Tech Stocks",
+        "description": "Stock & Crypto Holdings",
+        "holdings": {"AAPL,MSFT,AMZN,NVDA,BTC"}
+    }
+]
+#create Security list
+Securities = [
+    {
+        "symbol": "AAPL",
+        "name": "Apple Inc.",
+        "price": 250.00},
+    {
+        "symbol": "MSFT",
+        "name": "Microsoft Corporation",
+        "price": 500.00},
+    {
+        "symbol": "AMZN",
+        "name": "Amazon",
+        "price": 280.00},  
+    {
+        "symbol": "NVDA",
+        "name": "Microsoft Corporation",
+        "price": 200.00},
+    {
+        "symbol": "BTC",
+        "name": "Bitcoin",
+        "price": 105000}
+]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+# print menu
 from cli import constants
 from cli.menu_printer import print_menu
 
 print_menu(constants.login_menu)
+
+
+# should print login menu and should naviagate between menus based on user input
