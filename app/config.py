be7cff86b7c812e223c0ddd3386fc7694e4922f5
Kitchen Jr <e390944@us.lmco.com>
2025-12-16 17:22:27 -0600

    2.4 Configuration of database uri

--- app/config.py
+++ app/config.py
@@ -1,8 +1,12 @@
 def database_config():
-    user = "root"
-    password = "Cheeseit22"
-    host = "localhost"
-    port = "3306"
-    db_name = "kiwidb"
-    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+    return {
+    'user': "root",
+    'password': "Cheeseit22",
+    'host': "localhost",
+    'port': "3306",
+    'db_name': "kiwidb"
+    }
 
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{database_config.get('user')}:{database_config.get('password')}@{database_config.get('host')}:{database_config.get('port')}/{database_config.get('db_name')}"
