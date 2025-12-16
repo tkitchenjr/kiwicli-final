80ad9309c5024a6127017f8d5ee543d69117ed09
Kitchen Jr <e390944@us.lmco.com>
2025-12-16 16:01:14 -0600

    flask db object

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
-"""
-Application package marker so absolute imports like `app.services` work regardless
-of the working directory.
-"""
+from flask import Flask
+from app.db import db
 
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    
+    #register extensions
+    db.init_app(app)
+
+    return app
